3ea246b4580f71d1bb93bc117704f73159276d1e
Dhruv <hruv2004@gmail.com>
2024-07-22 14:18:45 +0530

    36-40

---
+++ src/com/company/java37.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class java37 {
+    public static void main(String[] args) {
+        System.out.println("four pillers of java::::::::::data abstraction--inheritance--encapsulation--polymorphism");
+    }
+}
---
+++ src/com/company/java39.java
@@ -0,0 +1,95 @@
+package com.company;
+import java.util.Scanner;
+
+
+class emp {
+    int salary;
+    String name;
+
+    public int getsalary() {
+        return salary;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public void setname(String s) {
+        name = s;
+        System.out.println(s);
+    }
+}
+
+
+
+
+class cellphone{
+
+    public void ringing(){
+        System.out.println("ringing");
+    }
+    public void vibrate(){
+        System.out.println("vibrate");
+    }
+    public void tring(){
+        System.out.println("tring");
+    }
+
+}
+
+
+
+class sq{
+    int s;
+    public int ar(){
+        return s*s;
+    }
+    public int peri(){
+        return s*4;
+    }
+}
+
+
+class Tommy{
+    public void hit(){
+        System.out.println("Hitting the enemy");
+    }
+    public void run(){
+        System.out.println("Running from the enemy");
+    }
+    public void fire(){
+        System.out.println("Firing on the enemy");
+    }
+}
+
+class java39{
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner (System.in);
+
+//        emp on=new emp();
+//        on.name="kame";
+//        on.salary=22;
+//        System.out.println(on.getname());
+//        System.out.println(on.getsalary());
+//        on.setname("hame");
+//
+//        cellphone cp=new cellphone();
+//        cp.ringing();
+//        cp.vibrate();
+//        cp.tring();
+
+//        sq sq=new sq();
+//        System.out.println("side please:");
+//        sq.s=in.nextInt();
+//        System.out.println(sq.ar());
+//        System.out.println(sq.peri());
+
+
+//        Tommy player1 = new Tommy();
+//        player1.fire();
+//        player1.run();
+//        player1.hit();
+        }
+    }
+
